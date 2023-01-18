f4f97cd505f618e019a205166e061372ccebc446
izunarin <112565053+izunarin@users.noreply.github.com>
2023-01-18 22:59:15 +0000

    itemu途中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,14 +8,28 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
+  end
+  
+    private
+
+  def item_params
+    params.require(:item).permit(:introduction, :name, :genre, :weekday, :url)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   
-  enum genre: { payment_waiting: 0, payment_confirmation: 1,in_production: 2,preparing_delivery: 3,delivered: 4, }
+  enum genre: { spring: 0, summer: 1,fall: 2,winter: 3, }
+  
+  enum weekday: { monday: 0, tuesday: 1,wednesday: 2,thursday: 3,friday: 4,saturday: 5,sunday: 6, }
 end
